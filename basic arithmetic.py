737785fcb6e625e1de181b75ba749e41fc59cf5e
srilakshmi <srilakshmiprakash9@gmail.com>
2022-06-10 10:23:46 +0530

    arithmetic

--- basic arithmetic.py
+++ basic arithmetic.py
@@ -1,10 +1,6 @@
 a=int(input(' enter the first number'))
 b=int(input('enter the second number'))
-sum=a+b
-sub=a-b
-multiply=a*b
-division=a/b
-print(sum)
-print(sub)
-print(multiply)
-print(division)
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
